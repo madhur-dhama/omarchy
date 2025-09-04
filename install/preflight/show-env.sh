0b3ca99e98a124401a17959521113f48ba8f316f
madhur-dhama <madhurdhama@gmail.com>
2025-09-05 00:07:16 +0530

    set my git name and email

--- install/preflight/show-env.sh
+++ install/preflight/show-env.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export OMARCHY_USER_NAME="madhur-dhama"
+export OMARCHY_USER_EMAIL="madhurdhama@gmail.com"
+
 echo "Installation ENV:"
 env | grep -E "^(OMARCHY_CHROOT_INSTALL|OMARCHY_USER_NAME|OMARCHY_USER_EMAIL|USER|HOME|OMARCHY_REPO|OMARCHY_REF)="
